f73d1bc1f229b57e87b1caea13aaa8298fc3199f
JPmorenoH <juan.moreno@sourcemeridian.com>
2026-04-18 07:28:23 -0500

    edit deploy shh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ set -e
 
 cd artifacts/orsa-guatape
 
+echo "Deploying to Cloudflare Pages..."
+
 npx wrangler pages deploy dist/public \
   --project-name=orsa-travel \
-  --account-id=bfcf2211a8497d8334bc1faf7f9d1f28 \
-  --branch=main
+  --branch=main \
+  --commit-dirty=true
+
+echo "Deploy finished"
